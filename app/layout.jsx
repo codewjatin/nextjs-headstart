1b78b1d4475b39b5da8fdc8ddf7dd28a7b79cf34
Jatin Kumar <main.jatink@gmail.com>
2022-11-04 15:37:48 +0530

    Octocat hand animation and font changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,17 +1,17 @@
 import Footer from '../components/Footer';
 import Nav from '../components/Nav';
-import { Inter } from '@next/font/google';
+import { Outfit } from '@next/font/google';
 import cn from 'classnames';
 import './globals.css';
 
-const interFont = Inter({ display: 'swap', subsets: ['latin'] });
+const outfitFont = Outfit({ display: 'swap', subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
         className={cn(
-          interFont.className,
+          outfitFont.className,
           'flex justify-center items-center px-4'
         )}>
         <Nav />
